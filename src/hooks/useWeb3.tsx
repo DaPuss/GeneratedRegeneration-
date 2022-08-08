6901f67b1e58e37c8f538f37bf6010f53a82a8d7
Dyl <34264347+Dylan-Frewen@users.noreply.github.com>
2022-08-08 20:31:26 +1200

    bundle hooks together

---
+++ src/hooks/useWeb3.tsx
@@ -0,0 +1,60 @@
+import {useEffect} from 'react'
+import { useContractWrite } from 'wagmi'
+import contractInterface from '../abi/abi.json'
+import { useContractRead } from 'wagmi'
+import {  ethers, BigNumber } from 'ethers'
+//custom hook that returns a function to call the mint function of the contract using WAGMI hooks
+
+export const useWeb3 = () => {
+  const addressOrName = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+  const mintPrice = 0.09
+  const writeMint = useContractWrite({
+    addressOrName,
+    contractInterface,
+    functionName: 'mintMany',
+
+    onMutate({ args, overrides }) {
+      console.log('Mutate', { args, overrides })
+    },
+  })
+
+  const readTotalSupply = useContractRead({
+    addressOrName,
+    contractInterface,
+    functionName: 'totalSupply',
+  })
+
+  const getCurrentSupply = () => {
+    const currentSupply = readTotalSupply.isSuccess?  BigNumber.from(readTotalSupply.data).toNumber() : 0;
+    return currentSupply
+  }
+
+  const mintNft = (mintAmount: number) => {
+    if(mintAmount < 1) return;
+
+    const {write} = writeMint;
+    const totalCost = getMintPrice(mintAmount);
+    
+    write({
+      args: [mintAmount],
+      overrides: {
+        value: ethers.utils.parseEther(totalCost),
+      },
+    })
+  }
+
+  const getMintPrice = (mintAmount: number) => {
+    const totalPrice = (mintAmount * mintPrice * 100) / 100
+    return isNaN(totalPrice) ? '0.00' : totalPrice.toFixed(2)
+  }
+
+
+  useEffect(() => {
+    readTotalSupply.refetch()
+  }, [mintNft])
+  
+
+  const totalSupply = getCurrentSupply()
+
+  return {mintNft, getMintPrice, totalSupply}
+}
